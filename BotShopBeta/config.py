6f16e7e8eb6067777180ed059a0e07113964891c
VladRahimov <rahimovvladyslav190705@outlook.com>
2026-06-26 14:15:19 +0300

    Obfuscate sensitive tokens in config.py
    Replaced sensitive tokens with placeholders for security.

--- BotShopBeta/config.py
+++ BotShopBeta/config.py
@@ -1,12 +1,12 @@
-TOKEN = "8726680377:AAGzATeya8iBHq30Lr_9WmUcHgNG58-hFoM"
+TOKEN = "**************************************" #Your bot token
 
-CRYPTO_PAY_TOKEN = "589814:AAZ42qIs34R8CveQpIW7KyWbhyvED4N9Oxs"
+CRYPTO_PAY_TOKEN = "**************************************" #Your cryptopay token
 
 CRYPTO_PAY_API_URL = "https://pay.crypt.bot/api"
 
-ADMIN_CHAT_ID = -1003906256504
+ADMIN_CHAT_ID = -************* 
 
-REVIEW_CHAT_ID = -1003722615392
+REVIEW_CHAT_ID = -************* 
 
 PLATEGA_MERCHANT_ID = "cc22040e-63b3-4e4c-a209-54620725a75e"
 PLATEGA_SECRET = "OL8ZdtNNzyFUR8ndkxNllfx1UFgMHb3Jc7nDBpQTCI1V22lX85C1QE5EQYKooHxwkLKzkBR8ARQ0NZx0F7dA5skoTVWZeNWC2gWw"
@@ -18,5 +18,5 @@ SUPPORT_BOT = "https://t.me/ScoringPointsBot"
 REVIEWS_CHANNEL = "https://t.me/OtzivyShadowBR"
 
 ADMIN_IDS = [
-    1080583901, 6128596797
+    *********, *********
 ]
