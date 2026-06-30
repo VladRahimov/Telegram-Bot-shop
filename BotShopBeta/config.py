513fddbaf7c3ac54b93c627f9d5a75b980d7c5fd
VladRahimov <rahimovvladyslav190705@outlook.com>
2026-06-30 18:17:18 +0300

    Update config.py

--- BotShopBeta/config.py
+++ BotShopBeta/config.py
@@ -8,8 +8,8 @@ ADMIN_CHAT_ID = -*************
 
 REVIEW_CHAT_ID = -************* 
 
-PLATEGA_MERCHANT_ID = "cc22040e-63b3-4e4c-a209-54620725a75e"
-PLATEGA_SECRET = "OL8ZdtNNzyFUR8ndkxNllfx1UFgMHb3Jc7nDBpQTCI1V22lX85C1QE5EQYKooHxwkLKzkBR8ARQ0NZx0F7dA5skoTVWZeNWC2gWw"
+PLATEGA_MERCHANT_ID = "----------------------------------"
+PLATEGA_SECRET = "--------------------------------------------------------------"
 
 PLATEGA_API_URL = "https://app.platega.io"
 
